69442921aa33cf3133d87706a73431189e32bb86
Artiom <bolbatart@gmail.com>
2021-04-21 22:27:03 +0300

    added edit orifle dialog

--- src/components/Dialogs/EditProfileDialog.tsx
+++ src/components/Dialogs/EditProfileDialog.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 import Avatar from 'assets/images/avatar.jpg';
 
-import { Button, Modal } from 'reactstrap';
+import { Button, Input, Modal } from 'reactstrap';
 
 
 
@@ -16,14 +16,62 @@ const EditProfileDialog: React.FC<IProps> = (props) => {
   return (
     <Modal isOpen={props.isOpen} toogle={props.toogle} >
       <StyledEditProfileDialog>
-        <img src={Avatar} alt="" className='_avatar' />
-
-
-        <div className="epd-buttons">
-          <Button onClick={props.toogle}>
-            Cancel
-          </Button>
+        <div className="epd-avatar__container">
+          <img src={Avatar} alt="" className='epd__avatar _avatar' />
+          <Button outline>Change</Button>
         </div>
+
+        <form className="epd-form">
+          <div className="epd-form__item">
+            <label htmlFor="">First name</label>
+            <Input />
+          </div>
+
+          <div className="epd-form__item">
+            <label htmlFor="">Last name</label>
+            <Input />
+          </div>
+
+          <div className="epd-form__item">
+            <label htmlFor="">Job position</label>
+            <Input />
+          </div>
+
+          <div className="epd-form__item">
+            <label htmlFor="">Location</label>
+            <Input />
+          </div>
+
+          <div className="epd-form__item">
+            <label htmlFor="">Website</label>
+            <Input />
+          </div>
+
+          <div className="epd-form__item">
+            <label htmlFor="">Bio</label>
+            <Input />
+          </div>
+
+          <div className="epd-form__item">
+            <label htmlFor="">Password</label>
+            <Input />
+          </div>
+
+          <div className="epd-form__item">
+            <label htmlFor="">Confirm password</label>
+            <Input />
+          </div>
+
+          <div className="epd-form__buttons">
+            <Button onClick={props.toogle}>
+              Cancel
+            </Button>
+            <Button onClick={props.toogle}>
+              Save changes
+            </Button>
+          </div>
+        </form>
+
       </StyledEditProfileDialog>
     </Modal>
   )
